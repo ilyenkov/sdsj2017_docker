eec5ee22b595cf085e66c035eb23766a228a6774
ilyenkov <qwerty31@mail.ru>
2017-11-06 02:21:40 +0300

    7
    7

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,7 @@ FROM kaggle/python
 
 RUN pip install pymorphy2[fast] tqdm pymystem3
 RUN python -c "import pymystem3.mystem ; pymystem3.mystem.autoinstall()"
-RUN wget https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.ru.vec -O wikiru.vec
-RUN python -c "from gensim.models.keyedvectors import KeyedVectors ; w2v = KeyedVectors.load_word2vec_format("wikiru.vec") ; word_vecs = {} ; for word in w2v.vocab: ; vec = w2v[word]; word_vecs[word] = vec ; with open("wikiru.vec.txt", "wt") as outfile: ; for word, vec in word_vecs.items(): ; vec = " ".join([str(i) for i in vec]) ; wordvec = " ".join([word, vec]) ; outfile.write(wordvec + "\n")"
+RUN wget https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.ru.vec -O wiki.ru.vec
+RUN wget https://github.com/ilyenkov/sdsj2017_docker/raw/master/make_vectors_txt.py
+RUN python make_vectors_txt.py
+RUN rm wiki.ru.vec
---
+++ make_vectors_txt.py
@@ -0,0 +1,32 @@
+from gensim.models.keyedvectors import KeyedVectors
+
+w2v = KeyedVectors.load_word2vec_format("wiki.ru.vec")
+
+word_vecs = {}
+for word in tqdm(w2v.vocab):
+    if len(word.split()) == 1:
+        vec = w2v[word]
+        word_vecs[word] = vec
+
+with open("wiki.ru.vec.txt", "wt") as outfile:
+    for word, vec in tqdm(word_vecs.items()):
+        vec = " ".join([str(i) for i in vec])
+        wordvec = " ".join([word, vec])
+        outfile.write(wordvec + "\n")
+
+char_vecs = {}
+for word in tqdm(w2v.vocab):
+    vec = w2v[word]
+    if len(word.split()) == 1:
+        for char in word:
+            if char in char_vecs:
+                char_vecs[char] = (char_vecs[char][0] + vec, char_vecs[char][1] + 1)
+            else:
+                char_vecs[char] = (vec, 1)
+
+with open("wiki.ru.vec_char_emb.txt", "wt") as outfile:
+    for char, vec in tqdm(char_vecs.items()):
+        avg_vector = np.round((vec[0] / vec[1]), 6).tolist()
+        avg_vector = " ".join([str(i) for i in avg_vector])
+        charvec = " ".join([char, avg_vector])
+        outfile.write(charvec + "\n")
